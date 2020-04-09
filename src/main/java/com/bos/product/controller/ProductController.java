ed48da8eccaf2386f77dd9e97ca5ee637cd83a96
Steven Kristianto <stevenkristianto97@yahoo.com>
2020-04-09 12:24:44 +0700

    remove cross origin

--- src/main/java/com/bos/product/controller/ProductController.java
+++ src/main/java/com/bos/product/controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/bos", produces = "application/json")
-@CrossOrigin(origins = {"*"})
 public class ProductController {
     @Autowired
     ProductService g_productService;
