b3fc1ea3790a63ac1c6f74f4b7be10b2fd4a358e
Steven Kristianto <stevenkristianto97@yahoo.com>
2020-04-13 11:10:48 +0700

    bug fixing encoder

--- src/main/java/com/bos/product/service/ProductService.java
+++ src/main/java/com/bos/product/service/ProductService.java
@@ -287,7 +287,9 @@ public class ProductService {
 
     public String encoder(String p_imagePath) {
         String tmp_base64Image = "";
-        File tmp_file = new File(p_imagePath);
+        System.out.println(p_imagePath.substring(8));
+        String imagePath = "\\NASBOS\\" + p_imagePath.substring(8);
+        File tmp_file = new File(imagePath);
         try (FileInputStream tmp_imageInFile = new FileInputStream(tmp_file)) {
             // Reading a Image file from file system
             byte tmp_imageData[] = new byte[(int) tmp_file.length()];
